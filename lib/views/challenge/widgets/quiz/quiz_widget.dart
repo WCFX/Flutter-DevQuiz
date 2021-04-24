633fb0c78ad65fae17c520c0f535a5dd676aed00
wcfx <wcfx.dev@gmail.com>
2021-04-23 21:37:45 -0300

    feat: quiz com multipla escolha funcionando e indicando qual e a correta

--- lib/views/challenge/widgets/quiz/quiz_widget.dart
+++ lib/views/challenge/widgets/quiz/quiz_widget.dart
@@ -1,12 +1,22 @@
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/models/answer_model.dart';
 import 'package:dev_quiz/models/question_model.dart';
-import 'package:dev_quiz/views/challenge/answer/answer_widgwt.dart';
+import 'package:dev_quiz/views/challenge/answer/answer_widget.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
 
   const QuizWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +28,22 @@ class QuizWidget extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
             child: Text(
-              question.title,
+              widget.question.title,
               style: AppTextStyles.heading,
             ),
           ),
           SizedBox(
             height: 24,
           ),
-          ...question.answers
-              .map((e) => AnswerWidget(
-                    isRight: e.isRight,
-                    title: e.title,
-                  ))
-              .toList()
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: answer(i),
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+              },
+            ),
         ],
       ),
     );
